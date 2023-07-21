48d35a2d30407c6332064930ad83dfbd350bf7eb
Asilbek Makhmudov <Asilbek_Makhmudov@epam.com>
2023-07-21 15:43:00 +0500

    added login test steps

--- src/test/java/com/exode/ui/pages/LoginPage.java
+++ src/test/java/com/exode/ui/pages/LoginPage.java
@@ -1,10 +1,14 @@
 package com.exode.ui.pages;
 
+import com.exode.ui.driver.DriverSingleton;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
 public class LoginPage extends AbstractPage{
 
+    public LoginPage() {
+        DriverSingleton.getDriver().get("https://staging.exode.ru/login");
+    }
     @FindBy(xpath = "//button[@data-test='auth.login.loginViaEmail']")
     WebElement emailLoginButton;
     @FindBy(xpath = "//input[@name='login']")
@@ -14,6 +18,10 @@ public class LoginPage extends AbstractPage{
     @FindBy(xpath = "//input[@name='password']")
     WebElement passwordInputField;
 
+    public LoginPage clickLoginWithEmailButton() {
+        wait.waitUntilClickable(emailLoginButton).click();
+        return this;
+    }
     public LoginPage inputEmail() {
         wait.waitUntilVisible(emailInputField).sendKeys("abek.makhmudov@gmail.com");
         return this;
--- src/test/java/com/exode/ui/tests/SettingsTest.java
+++ src/test/java/com/exode/ui/tests/SettingsTest.java
@@ -1,8 +1,24 @@
 package com.exode.ui.tests;
 
+import com.exode.ui.driver.DriverSingleton;
+import com.exode.ui.pages.LoginPage;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class SettingsTest {
 
+    @BeforeClass
+    void setup() {
+        DriverSingleton.getDriver();
+    }
+    @AfterClass
+    void tearDown() {
+        DriverSingleton.closeDriver();
+    }
 
+    @Test
+    public void loginWithValidCredentials() {
+        new LoginPage().clickLoginWithEmailButton().inputEmail().clickSubmitButton().inputPassword().clickLogInButton();
+    }
 }
